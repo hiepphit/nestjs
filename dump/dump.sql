66fd3ca405bf9462ae894152ae0b71176ec88309
Hiep <hiepph.coder@gmail.com>
2021-07-28 01:45:17 +0700

    sql

--- dump/dump.sql
+++ dump/dump.sql
@@ -1,6 +1,5 @@
-CREATE TABLE IF NOT EXIST users (
-  id int(11) AUTO_INCREMENT,
-  username char not null,
-  `password` varchar not null,
+CREATE TABLE users (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  username varchar(50) not null,
+  `password` varchar(50) not null
 );
-Insert into users (id,username,`password`) values (1, 'admin', 'admin');
