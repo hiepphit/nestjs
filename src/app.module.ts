ea9fef9f4892e7c6437a538ba82d6a4ecd710fab
Hiep <hiepph.coder@gmail.com>
2021-07-28 22:56:00 +0700

    genarate resource category --no-spec by mac

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/user.module';
+import { CategoryModule } from './category/category.module';
+import { CategoryModule } from './category/category.module';
 @Module({
   imports: [
     ProductModule,
